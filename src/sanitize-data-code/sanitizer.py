5f30cbbd6af3ccfe4d190347ccaa7347fd68f3fa
Kartikeya Bhardwaj <kjbhardw@uwaterloo.ca>
2021-12-21 00:19:26 +0400

    sanitize

--- src/sanitize-data-code/sanitizer.py
+++ src/sanitize-data-code/sanitizer.py
@@ -1,12 +1,27 @@
 import csv
 
+newcsv = open('new_names.csv')
+
 with open('names.csv') as csvfile:
     csvreader = csv.reader(csvfile)
+    csvwriter = csv.writer(newcsv)
     lines= list(csvreader)
     count = 0
     bcountry = []
     for line in lines:
+        count += 1
+        newLine = line
         bcvars = line[7].split(',')
-        bcountry.append(bcvars[len(bcvars)-1])
-
-    print(bcountry)
+        countryName = bcvars[len(bcvars)-1]
+        countryName = countryName.strip() # Remove front and back whitespaces
+        if "[now " in countryName:
+            newName = countryName.split("now ")
+            newName = newName[1]
+            if "]" not in newName:
+                print(newName)
+            newName = newName.split("]")
+            newName = newName[0]
+            countryName = newName
+        newLine[7] = countryName
+        if (count < 3):
+            print(newLine)
