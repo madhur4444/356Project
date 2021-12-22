9f89f1c1da37c98ff52121c87aec1896a1614268
madhur4444 <mk3sharm@uwaterloo.ca>
2021-12-22 18:36:26 +0530

    update

--- src/client-code/server.py
+++ src/client-code/server.py
@@ -51,8 +51,9 @@ def update(table, fields, newVals, condition):
 
 def buildResponseObj(headings, rawResponse):
     response = {}
+    # 
     for h in headings:
-        response[h] = []
+        response[h[0]] = []
 
     # For each row in the response
     for row in rawResponse:
@@ -127,24 +128,24 @@ def addMovieToMovies(parts, cnx: MySQLConnection):
     query = "SELECT * FROM Movies;"
     queryParams = []
     response = getQueryResponse(cnx, query, [
-    ["imdb_title_id"],
-	["title"],
-    ["original_title"],
-	["year_of_release"],
-    ["date_published"],
-	["duration"],
-	["country"],
-	["language"],
-	["director"],
-	["writer"],
-	["production_company"],
-	["actors"],
-	["description"],
-	["budget"],
-	["usa_gross_income"],
-	["worldwide_gross_income"],
-	["reviews_from_users"],
-	["reviews_from_critics"]
+    "imdb_title_id",
+	"title",
+    "original_title",
+	"year_of_release",
+    "date_published",
+	"duration",
+	"country",
+	"language",
+	"director",
+	"writer",
+	"production_company",
+	"actors",
+	"description",
+	"budget",
+	"usa_gross_income",
+	"worldwide_gross_income",
+	"reviews_from_users",
+	"reviews_from_critics"
 ], queryParams)
     return 'nodata' in response
 
