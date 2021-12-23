d1ca19385b29f8748f37e72082f8b8dd4c6e67f5
Harshit Manchanda <hmanchanda27@gmail.com>
2021-12-23 14:39:55 +0100

    fixed problem

--- src/client-code/testServer.py
+++ src/client-code/testServer.py
@@ -33,7 +33,7 @@ def main():
             "Language": ["English"],
             "Director(s)": ["Jon Watts"],
             "Writer(s)": [""],
-            "Production Company": ["Marvel Studios"]
+            "Production Company": ["Marvel Studios"],
             "Actor(s)": ["Tom Holland, Zendaya, Benedict Cumberbatch, Jacob Batalon, Jon Favreau"],
             "Description": ["Spider-Man: No Way Home is a 2021 American superhero film based on the Marvel Comics character Spider-Man, co-produced by Columbia Pictures and Marvel Studios and distributed by Sony Pictures Releasing. It is the sequel to Spider-Man: Homecoming (2017) and Spider-Man: Far From Home (2019), and is the 27th film in the Marvel Cinematic Universe (MCU)."],
             "Budget": ["$200,000,000"],
